c9f1dbdcb4b037fe58db9a286f98b1d515577187
smirnp <smirnp@gmail.com>
2018-02-13 10:19:28 +0100

    Separated constants and DockerFileBuilder

--- src/main/java/org/hobbit/sdk/examples/examplebenchmark/Constants.java
+++ src/main/java/org/hobbit/sdk/examples/examplebenchmark/Constants.java
@@ -1,4 +1,4 @@
-package org.hobbit.sdk.examples.examplebenchmark.docker;
+package org.hobbit.sdk.examples.examplebenchmark;
 
 
 import org.hobbit.sdk.docker.builders.DynamicDockerFileBuilder;
@@ -7,7 +7,7 @@ import org.hobbit.sdk.docker.builders.DynamicDockerFileBuilder;
  * @author Pavel Smirnov
  */
 
-public class ExampleDockersBuilder extends DynamicDockerFileBuilder {
+public class Constants {
 
     public static String GIT_REPO_PATH = "git.project-hobbit.eu:4567/";
     //public static String GIT_REPO_PATH = "";
@@ -25,19 +25,8 @@ public class ExampleDockersBuilder extends DynamicDockerFileBuilder {
     public static final String BENCHMARK_URI = "http://project-hobbit.eu/"+PROJECT_NAME;
     public static final String SYSTEM_URI = "http://project-hobbit.eu/"+PROJECT_NAME+"system";
 
-    public ExampleDockersBuilder(Class runnerClass, String imageName) throws Exception {
-        super("ExampleDockersBuilder");
-        imageName(imageName);
-        //user-friendly name for searching in logs
-        containerName(runnerClass.getSimpleName());
-        //temp docker file will be created there
-        buildDirectory(".");
-        //should be packaged will all dependencies (via 'mvn package' command)
-        jarFilePath("target/sdk-example-benchmark-1.0.jar");
-        //will be placed in temp dockerFile
-        dockerWorkDir("/usr/src/"+PROJECT_NAME);
-        //will be placed in temp dockerFile
-        runnerClass(org.hobbit.core.run.ComponentStarter.class, runnerClass);
-    }
+    public static final String SDK_BUILD_DIR_PATH = ".";  //build directory, temp docker file will be created there
+    public static final String SDK_JAR_FILE_PATH = "target/sdk-example-benchmark-1.0.jar"; //should be packaged will all dependencies (via 'mvn package -DskipTests=true' command)
+    public static final String SDK_WORK_DIR_PATH = "/usr/src/"+PROJECT_NAME;
 
 }
